15a1054a3d9aef4607ae25de9b01227849fadff8
DungNQ <nguyen.quang.dung@framgia.com>
2013-10-01 08:19:29 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DailyReportManagement::Application.config.secret_key_base = '80caa529586580e03fb468a2262381f6a3fcecae98d2c9b47ca2a02b186033b25881091e7db9b0184940d093e6ec13578a370e17d3e4d511b490a107d713c9e8'
